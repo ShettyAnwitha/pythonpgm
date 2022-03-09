4c49a80e6ec4227647b1aab8d3e468d1200620a4
ShettyAnwitha <98326705+ShettyAnwitha@users.noreply.github.com>
2022-03-09 23:40:20 +0530

    Add files via upload

---
+++ project1/multiplicationtable.py
@@ -0,0 +1,12 @@
+print("\t\t\t\tgenerating multiplication table from 3 to 20\n")
+
+for i in range (3,21):
+    print(i, end="\t")
+print()
+print("___________________________________________________________\n")
+
+for j in range(3,21) :
+    for k in range(1,11):
+        print(j * k, ' \t' ,)
+    print("\n")
+    
